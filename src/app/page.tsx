7cb3dd391faa4dbc8b249e96f9cf17574f0c57a5
Vinicius <ewerton.viniciusturco@gmail.com>
2023-08-31 19:43:28 -0300

    :sparkles: feat: criação da estrutura da página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-export const Home = () => {
-  return <></>;
+import { Divider, Stack } from "@chakra-ui/react";
+import { Content } from "@/layout/content";
+import { Header } from "@/layout/header";
+import { ButtonTheme } from "@/components/button/buttonTheme";
+import { Footer } from "@/layout/footer";
+
+const Page = () => {
+  return (
+    <Stack maxH={"full"} maxW={"full"}>
+      <ButtonTheme />
+      <Stack paddingLeft={"10%"} paddingRight={"10%"}>
+        <Header />
+        <Divider />
+        <Content />
+        <Divider />
+        <Footer />
+      </Stack>
+    </Stack>
+  );
 };
 
-export default Home;
+export default Page;
